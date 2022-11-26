53e5abda34460debfd60a9b2eeee6f92a4e8a275
yama-t <yamato.ybrik555@gmail.com>
2022-11-26 14:58:34 +0900

    ヘッダーとグラフのコンポーネントを画面に読み込ませる。

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import PageHeader from "@/components/PageHeader";
+import PopuLationGraph from "./PopulationGraph";
+
+const pageTitle = "都道府県別の総人口推移グラフ";
 
 export default function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <PageHeader title={pageTitle} />
+      <PopuLationGraph />
+    </div>
+  );
 }
