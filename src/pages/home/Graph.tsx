f0d189d9d2e461a32ce68da9564a935506b6be6f
yama-t <yamato.ybrik555@gmail.com>
2022-11-27 13:20:56 +0900

    空のグラフを表示する

--- src/pages/home/Graph.tsx
+++ src/pages/home/Graph.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+const graphTitle = "";
+const seriesDefaultName = "都道府県名";
+const xAxisText = "年度";
+const yAxisText = "人口数";
 
 export default function Graph() {
-  return <div className="graph"></div>;
+  const categories: string[] = [];
+  const series: Highcharts.SeriesOptionsType[] = [
+    { type: "line", name: seriesDefaultName, data: [] },
+  ];
+  const options: Highcharts.Options = {
+    title: { text: graphTitle },
+    xAxis: { title: { text: xAxisText }, categories },
+    yAxis: { title: { text: yAxisText } },
+    series,
+  };
+
+  return (
+    <div className="graph">
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
 }
