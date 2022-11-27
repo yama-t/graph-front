cad995ac784787060c1e7db1ec20c20caf482097
yama-t <yamato.ybrik555@gmail.com>
2022-11-27 15:18:18 +0900

    チェックボックスを付けた都道府県の人口推移を取得する処理の追加

--- src/pages/home/PopulationGraph.tsx
+++ src/pages/home/PopulationGraph.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Prefectures, PrefCodes } from "@/types/prefecture";
+import { Prefectures, PrefCodes, PrefCode } from "@/types/prefecture";
 import PrefectureCheckbox from "./PrefecturesCheckbox";
 import Graph from "./Graph";
 
 const apiEndPoint = "https://opendata.resas-portal.go.jp";
+const apiKey = import.meta.env.VITE_RESAS_API_KEY;
 const getPrefecturesApi = "/api/v1/prefectures";
 const getPrefecturesUrl = apiEndPoint + getPrefecturesApi;
+const getPopulationApi = "/api/v1/population/composition/perYear";
+const getPopulationUrl = apiEndPoint + getPopulationApi;
 
 export default function PopuLationGraph() {
   const [prefectures, setPrefectures] = useState<Prefectures>([]);
@@ -13,25 +16,42 @@ export default function PopuLationGraph() {
 
   const getPrefectures = async () => {
     const response = await fetch(getPrefecturesUrl, {
-      headers: { "X-API-KEY": import.meta.env.VITE_RESAS_API_KEY },
+      headers: { "X-API-KEY": apiKey },
     });
     const jsonData = await response.json();
     setPrefectures(jsonData.result);
   };
 
-  useEffect(() => {
-    getPrefectures();
-  }, []);
+  const getPopulation = async (prefCode: PrefCode) => {
+    const params = new URLSearchParams({ prefCode: `${prefCode}` });
+    const getUrl = `${getPopulationUrl}?${params}`;
+    const response = await fetch(getUrl, {
+      headers: { "X-API-KEY": apiKey },
+    });
+    const jsonData = await response.json();
+  };
 
   const checkPrefecture = function (input: string) {
     const changeCode = Number(input);
-    const newPrefCodes = prefCodes.includes(changeCode)
+    // チェックを外した場合：true（配列に既に含まれている）
+    // チェックを付けた場合：false（配列に含まれていない）
+    const isIncludes = prefCodes.includes(changeCode);
+    const newPrefCodes = isIncludes
       ? // チェックした値が既に含まれている場合は、保持している配列から取り除く
         prefCodes.filter((item) => item !== changeCode)
       : // 含まれていない場合は、保持している配列に追加する
         [...prefCodes, changeCode];
     setPrefCodes(newPrefCodes);
+
+    // チェックを付けた場合だけ人口を取得する（外した場合は動作しない）
+    if (!isIncludes) {
+      getPopulation(changeCode);
+    }
   };
+
+  useEffect(() => {
+    getPrefectures();
+  }, []);
 
   return (
     <>
