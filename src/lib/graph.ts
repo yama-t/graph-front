bb955da4af6dc02eb15bbe071838b6e420ac8cf7
yama-t <yamato.ybrik555@gmail.com>
2022-11-29 16:34:40 +0900

    グラフに適用するデータ生成の処理を別ファイル化

---
+++ src/lib/graph.ts
@@ -0,0 +1,59 @@
+import Highcharts from "highcharts";
+import { PrefecturePopulation } from "@/types/prefecture";
+
+interface PopulationData {
+  categories: Highcharts.XAxisOptions["categories"];
+  series: Highcharts.SeriesOptionsType[];
+}
+
+/*
+## categories
+["1960", "1965", "1970", "1975", "1980"]...
+
+## series
+ [
+    {
+        data: [9683802, 10869244...]
+        name: "東京都"
+        type: "line"
+    },
+    {
+        data: [3443176, 4430743,...]
+        name: "神奈川県"
+        type: "line"
+    }
+    ...
+ ]
+*/
+const createPopulationData = function (
+  rawData: PrefecturePopulation[],
+  seriesDefaultName = ""
+): PopulationData {
+  // X軸（年）
+  const categories: Highcharts.XAxisOptions["categories"] = [];
+  // Y軸（人口）
+  let series: Highcharts.SeriesOptionsType[] = [];
+
+  for (const rd of rawData) {
+    const data = [];
+
+    for (const population of rd.data) {
+      data.push(population.value);
+      categories.push(String(population.year));
+    }
+
+    series.push({
+      type: "line",
+      name: rd.prefName,
+      data,
+    });
+  }
+
+  if (series.length === 0) {
+    series = [{ type: "line", name: seriesDefaultName, data: [] }];
+  }
+
+  return { categories, series };
+};
+
+export { createPopulationData };
--- src/pages/home/Graph.tsx
+++ src/pages/home/Graph.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import { PrefecturePopulation } from "@/types/prefecture";
+import { createPopulationData } from "@/lib/graph";
 
 const graphTitle = "";
 const seriesDefaultName = "都道府県名";
@@ -33,62 +34,8 @@ interface Props {
   data: PrefecturePopulation[];
 }
 
-interface PopulationData {
-  categories: Highcharts.XAxisOptions["categories"];
-  series: Highcharts.SeriesOptionsType[];
-}
-
-/*
-## categories
-["1960", "1965", "1970", "1975", "1980"]...
-
-## series
- [
-    {
-        data: [9683802, 10869244...]
-        name: "東京都"
-        type: "line"
-    },
-    {
-        data: [3443176, 4430743,...]
-        name: "神奈川県"
-        type: "line"
-    }
-    ...
- ]
-*/
-const createPopulationData = function (
-  rawData: PrefecturePopulation[]
-): PopulationData {
-  // X軸（年）
-  const categories: Highcharts.XAxisOptions["categories"] = [];
-  // Y軸（人口）
-  let series: Highcharts.SeriesOptionsType[] = [];
-
-  for (const rd of rawData) {
-    const data = [];
-
-    for (const population of rd.data) {
-      data.push(population.value);
-      categories.push(String(population.year));
-    }
-
-    series.push({
-      type: "line",
-      name: rd.prefName,
-      data,
-    });
-  }
-
-  if (series.length === 0) {
-    series = [{ type: "line", name: seriesDefaultName, data: [] }];
-  }
-
-  return { categories, series };
-};
-
 export default function Graph({ data }: Props) {
-  const { categories, series } = createPopulationData(data);
+  const { categories, series } = createPopulationData(data, seriesDefaultName);
   const options: Highcharts.Options = {
     title: { text: graphTitle },
     chart: {
